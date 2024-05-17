0fc6721637a045048a0bee933767eaefaac906b2
jtlaquec <78872016+jtlaquec@users.noreply.github.com>
2024-05-17 08:46:56 -0500

    Dashboard update

--- app/controllers/DashboardController.php
+++ app/controllers/DashboardController.php
@@ -4,13 +4,29 @@ namespace App\Controllers;
 
 use Core\Controller;
 
+use App\Models\Personal;
+use App\Models\Viatico;
+use App\Models\Banco;
+use App\Models\CondicionLaboral;
 class DashboardController extends Controller {
 
 
 
     public function index() {
         $nombre = 'Dashboard';
-        $titulo = 'Panel Administrativo';
-        return $this->view('dashboard/index', compact('nombre', 'titulo'),true);
+        $titulo = 'Bienvenido, '.($_SESSION['username']);
+        $empleados = new Personal();
+        $empleados = count($empleados->index());
+
+        $viaticos = new Viatico();
+        $viaticos = count($viaticos->index());
+
+        $bancos = new Banco();
+        $bancos = $bancos->index();
+
+        $condiciones_laborales = new CondicionLaboral();
+        $condiciones_laborales = $condiciones_laborales->index();
+
+        return $this->view('dashboard/index', compact('nombre', 'titulo','empleados','viaticos','bancos','condiciones_laborales'),true);
     }
 }
--- app/views/dashboard/index.php
+++ app/views/dashboard/index.php
@@ -1,15 +1,68 @@
 <div>
 
-
     <div class="container mt-5 text-dark">
-        <h1>Bienvenido <?php echo htmlspecialchars($_SESSION['username']); ?></h1>
-        <p>Seleccione un nivel del menú para gestionar.</p>
 
-        <hr>
-        <p>Niveles de acceso (Prueba)</p>
-        <?php
-        print_r($_SESSION['niveles']);
-        ?>
+
+        <div class="d-flex justify-content-around">
+            <div class="card bg-light mb-3" style="max-width: 18rem;">
+                <div class="card-body">
+                    <h1 class="card-title center"><?php echo $empleados; ?></h1>
+                    <p class="card-text center">Empleados Registrados</p>
+                </div>
+            </div>
+            <div class="card bg-light mb-3" style="max-width: 18rem;">
+                <div class="card-body">
+                    <h1 class="card-title center"><?php echo $viaticos; ?></h1>
+                    <p class="card-text center">Viáticos Registrados</p>
+                </div>
+            </div>
+
+            <div class="card bg-light mb-3" style="max-width: 18rem;">
+                <div class="card-body">
+                    <h1 class="card-title center">Bancos</h1>
+                    <p class="card-text center">
+                    <ul>
+                        <?php
+                        if (!empty($bancos)) {
+                            foreach ($bancos as $banco) {
+                                echo '<li>' . htmlspecialchars($banco['banco']) . '</li>';
+                            }
+                        } else {
+                            echo ' <li>No hay bancos disponibles</li>';
+                        }
+                        ?>
+                    </ul>
+
+                    </p>
+                </div>
+            </div>
+
+            <div class="card bg-light mb-3" style="max-width: 18rem;">
+                <div class="card-body">
+                    <h1 class="card-title center">Condiciones Laborales</h1>
+                    <p class="card-text center">
+                    <ul>
+                        <?php
+                        if (!empty($condiciones_laborales)) {
+                            foreach ($condiciones_laborales as $condicion_laboral) {
+                                echo '<li>' . htmlspecialchars($condicion_laboral['condicion_laboral']) . '</li>';
+                            }
+                        } else {
+                            echo ' <li>No hay Condiciones Laborales disponibles</li>';
+                        }
+                        ?>
+                    </ul>
+
+                    </p>
+                </div>
+            </div>
+        </div>
+
+
+
+
+
+
     </div>
 
 
